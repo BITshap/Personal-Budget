de237251721950126ea08e3682ddb10a18f9d530
Nick Shapoff <nicholasshapoff@gmail.com>
2022-06-21 21:02:57 -0700

    MyBudgetTracker

--- index.js
+++ index.js
@@ -2,4 +2,31 @@ let weeklyExpenses = 0
 let monthlyExpenses = 0
 let annualExpenses = 0
 
-// Your Code Here
+//console.log(weeklyExpenseQuestions)
+
+/* window.prompt(weeklyExpenseQuestions[0]);
+
+window.prompt(weeklyExpenseQuestions[1]);
+
+window.prompt(weeklyExpenseQuestions[2]); */
+
+/*for (let i = 0; i < weeklyExpenseQuestions.length; i++){
+  let useranswer = parseInt(window.prompt(weeklyExpenseQuestions[i]))
+  weeklyExpenses = useranswer + weeklyExpenses;
+  console.log(weeklyExpenses);
+} */
+for (let i = 0; i < weeklyExpenseQuestions.length; i++){
+weeklyExpenses = weeklyExpenses + parseInt(window.prompt(weeklyExpenseQuestions[i]))
+console.log(weeklyExpenses) 
+}
+
+for (let i = 0; i < monthlyExpenseQuestions.length; i++){
+monthlyExpenses = monthlyExpenses + parseInt(window.prompt(monthlyExpenseQuestions[i])) 
+console.log(monthlyExpenses) 
+}
+
+for (let i = 0; i < annualExpenseQuestions.length; i++){
+annualExpenses = annualExpenses + parseInt(window.prompt(annualExpenseQuestions[i])) 
+console.log(annualExpenses) 
+}
+    
